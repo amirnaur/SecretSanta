30728e2b25af4a539c52bbdbd146fb2f863ebdfa
amir <amir.naur@gmail.com>
2021-12-07 16:04:00 +0600

    members distribution func

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -5,6 +5,7 @@ import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import { IconButton } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
+import { withTheme } from '@emotion/react';
 const initialValues = {
   members: [
     {
@@ -26,6 +27,7 @@ const GenerateSantas = () => (
       onSubmit={async (values) => {
         await new Promise((r) => setTimeout(r, 500));
         alert(JSON.stringify(values, null, 2));
+        distributeMembers(values['members']);
       }}
     >
       {({ values, setFieldValue }) => (
@@ -42,6 +44,8 @@ const GenerateSantas = () => (
                           label="Name"
                           placeholder="Jane Doe"
                           type="text"
+                          required
+                          autoComplete="off"
                           onChange={e => setFieldValue(`members.${index}.name`, e.target.value)}
                         />
                         <IconButton aria-label="delete" onClick={() => remove(index)}>
@@ -88,5 +92,39 @@ const GenerateSantas = () => (
     </Formik>
   </div>
 );
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+}
+const distributeMembers = (members) => {
+  const result = {};
+  let from = [...members];
+  let destiny = [...members];
+  // console.log(destiny[2]);
 
+  for(let i = 0; i < members.length; i++) {
+    console.log(from[0].name)
+    console.log("from[1]", from[1].name)
+    if(from.length === 2 && destiny.indexOf(from[0] >= 0)) {
+      console.log("i= ", i)
+      result[from[0].name] = destiny[0 ? destiny.indexOf(from[0]): 1].name;
+      from.splice(0, 1);
+      destiny.splice(destiny.indexOf(from[1]), 1);
+      result[from[0].name] = destiny[0].name;
+      break;
+    }
+    let rd_index = getRandomInt(0, from.length);
+    while(destiny[rd_index] === from[0]) {
+      rd_index = getRandomInt(0, from.length);
+    }
+    result[from[0].name] = destiny[rd_index].name;
+    from.splice(0, 1);
+    destiny.splice(rd_index, 1);
+  }
+  console.log(result)
+  return result;
+  
+
+}
 export default GenerateSantas;
